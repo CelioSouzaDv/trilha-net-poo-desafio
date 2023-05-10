32527c13aa69671bdcc00e556c37f07611140a8a
CelioSouzaDv <127158626+CelioSouzaDv@users.noreply.github.com>
2023-05-10 14:53:55 -0300

    Commit de resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12998958745", modelo: "Tijolao", imei: "2589-7364", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("###########################################");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone Iphone = new Iphone(numero: "12987254613", modelo: "Iphone 15x pro Max", imei: "2580-7913", memoria: 256);
+Iphone.Ligar();
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Clash of Clans");
